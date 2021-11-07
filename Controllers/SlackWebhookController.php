277e041d94754a4c42cb7710fb94bd0ae5ea4946
sugtao4423 <sugtao4423@gmail.com>
2021-11-08 02:31:21 +0900

    Slack slash commandsに対応！

---
+++ Controllers/SlackWebhookController.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../Services/ConfigService.php';
+require_once __DIR__ . '/../Services/DailyNoteService.php';
+require_once __DIR__ . '/../Services/OutputFormatService.php';
+
+class SlackWebhookController
+{
+    protected string $command;
+    protected string $gaveSlackUserId;
+    protected string $gaveText;
+
+    public function __construct()
+    {
+        global $_POST;
+        $this->command = $_POST['command'];
+        $this->gaveSlackUserId = $_POST['user_id'];
+        $this->gaveText = $_POST['text'];
+
+        $this->echoJson();
+    }
+
+    private function getUser(): ?Config
+    {
+        if (trim($this->gaveText) !== '') {
+            return (new ConfigService())->getUserByAlias($this->gaveText);
+        }
+        return (new ConfigService())->getUserBySlackUserId($this->gaveSlackUserId);
+    }
+
+    private function echoJson(): void
+    {
+        $user = $this->getUser();
+        if ($user === null) {
+            echo 'User not found.' . PHP_EOL;
+            exit(1);
+        }
+
+        $dailyNote = (new DailyNoteService())->getDailyNote($user);
+        if ($dailyNote === null) {
+            echo 'Failed to get daily note.' . PHP_EOL;
+            exit(1);
+        }
+
+        $outputFormatService = new OutputFormatService($dailyNote);
+        $blocks = array_merge(
+            $this->getHeadBlock($user),
+            $outputFormatService->getSlackWebhookBlocks($this->command)
+        );
+
+        $data = ['response_type' => 'in_channel'];
+        if ($this->command === '/genshin' || $this->command === '/resin') {
+            $data['attachments'] = [
+                [
+                    'color' => $outputFormatService->getSlackAttachmentColor(),
+                    'blocks' => $blocks,
+                ]
+            ];
+        } else {
+            $data['blicks'] = $blocks;
+        }
+
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data);
+    }
+
+    private function getHeadBlock(Config $user): array
+    {
+        $headText = "<@{$this->gaveSlackUserId}> ";
+        if ($this->gaveSlackUserId === $user->getSlackUserId()) {
+            $headText .= 'Your data!';
+        } else {
+            $headText .= 'Data of ' . $user->getAlias();
+        }
+        return [
+            [
+                'type' => 'section',
+                'text' => [
+                    'type' => 'mrkdwn',
+                    'text' => $headText,
+                ],
+            ],
+        ];
+    }
+}
--- GenshinDailyNote.php
+++ GenshinDailyNote.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/Controllers/CliController.php';
+require_once __DIR__ . '/Controllers/SlackWebhookController.php';
 
 $isCli = (php_sapi_name() === 'cli');
+$isSlackWebhook = (isset($_POST['user_id']) && isset($_POST['command']));
 
 if ($isCli) {
     new CliController();
+} else if ($isSlackWebhook) {
+    new SlackWebhookController();
 }
